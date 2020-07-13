89f165fbca2b2ddd5ebc5ef69043afc3bb5af20a
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-07-13 13:40:42 +0100

    remove testdefer

--- src/api/providers/githubprovider/github_provider_test.go
+++ src/api/providers/githubprovider/github_provider_test.go
@@ -11,11 +11,3 @@ func TestGetAuthHeader(t *testing.T) {
 
 	assert.EqualValues(t, "token 123xyz", header)
 }
-
-func TestDefer(t *testing.T) {
-	defer t.Logf("1")
-	defer t.Logf("2")
-	defer t.Logf("3")
-
-	t.Logf("function's body")
-}
